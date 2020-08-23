bab0d471358f45e1a75a762dfad2d6f6c3e02f50
Misha Ripp <misharipp@mail.ru>
2020-08-23 15:15:32 +0300

    Добавил чтение файла в команды.

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import telebot
 
 token = open('token.txt').read()
 bot = telebot.TeleBot(token)
-
+message1 = ''
+print(message1)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start \n Вот какие команды я могу исполнять: \n /quotation - найти имя персонажа и главу в книге по введенной Вами цитате. \n /character - выделить как можно больше цитат персонажа в заданной главе.')
 
 @bot.message_handler(commands=['quotation'])
 def start_quotation(message):
-    bot.send_message(message.chat.id, 'Напишите Вашу цитату и возможно название книги.')
+    global message1
+    message1 = 'quotation'
+    bot.send_message(message.chat.id, 'Напишите Вашу цитату и, возможно, название книги.')
+
+@bot.message_handler(commands=['character'])
+def start_character(message):
+    global message1
+    message1 = 'character'
+    bot.send_message(message.chat.id, 'Напишите персонажа, и главу, и ,возможно, название книги.')
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
@@ -17,5 +26,9 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Привет! Как я могу Вам помочь?')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Пока, хорошего дня!')
-
+    elif message1 == 'quotation':
+        text = open('token.txt', encoding="utf-8").read()
+        print(text.lower().find(message.text.lower()))
+    elif message1 == 'character':
+        bot.send_message(message.chat.id, 'Персонаж')
 bot.polling()
