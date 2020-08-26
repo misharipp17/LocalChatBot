c94520867b914264b321f754aa1976afaea227a9
Misha Ripp <misharipp@mail.ru>
2020-08-26 11:41:46 +0300

    Добавил скачивание файла.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+import urllib.request
 
 token = open('token.txt').read()
 bot = telebot.TeleBot(token)
@@ -20,6 +21,21 @@ def start_character(message):
     message1 = 'character'
     bot.send_message(message.chat.id, 'Напишите персонажа, и главу, и ,возможно, название книги.')
 
+@bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
+def handle_text_doc(message):
+    if message1 == 'quotation':
+        document_id = message.document.file_id
+        file_info = bot.get_file(document_id)
+        downloaded_file = bot.download_file(file_id_info.file_path)
+        src = massage.document.file_name
+        with open('C:\Users\misha\PycharmProjects\QuotationChatBot' + "\" + src, 'wb') as new_file:
+            new_file.write(downloaded_file)    
+        #text = open('war-peace.txt', encoding="utf-8").read()
+    #elif message1 == 'character':
+    
+    else:
+        bot.send_message(message.chat.id, 'Извините, но Вы прислали это не вовремя.')
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет':
@@ -27,8 +43,8 @@ def send_text(message):
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Пока, хорошего дня!')
     elif message1 == 'quotation':
-        text = open('token.txt', encoding="utf-8").read()
-        print(text.lower().find(message.text.lower()))
+        text = open('war-peace.txt', encoding="utf-8").read()
+        #print(text.lower().find(message.text.lower()))
     elif message1 == 'character':
-        bot.send_message(message.chat.id, 'Персонаж')
+        text = open('war-peace.txt', encoding="utf-8").read()
 bot.polling()
