f8a3c64e29763818be5042d7e6609f41f06837d4
Misha Ripp <misharipp@mail.ru>
2020-08-26 13:58:28 +0300

    Исправлено скачивание файла из телеграмм, добавлена функция по поиску количества абзацев с данной цитатой

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import telebot
-import urllib.request
 
 token = open('token.txt').read()
 bot = telebot.TeleBot(token)
-message1 = ''
-print(message1)
+state = ''
+quotation = ''
+character = ''
+print(state)
+def search_qoutation(quotation, file_name): 
+    """получает цитату и файл с книжкой, возвращает список абзацев с данной цитатой"""
+    paragraph = []
+    for line in open(file_name, encoding="utf-8"):
+        number_quotation = line.lower().find(quotation.lower())
+        if number_quotation != -1:
+            paragraph.append(line)
+    return paragraph
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start \n Вот какие команды я могу исполнять: \n /quotation - найти имя персонажа и главу в книге по введенной Вами цитате. \n /character - выделить как можно больше цитат персонажа в заданной главе.')
 
 @bot.message_handler(commands=['quotation'])
 def start_quotation(message):
-    global message1
-    message1 = 'quotation'
+    global state
+    state = 'quotation'
     bot.send_message(message.chat.id, 'Напишите Вашу цитату и, возможно, название книги.')
 
 @bot.message_handler(commands=['character'])
 def start_character(message):
-    global message1
-    message1 = 'character'
+    global state
+    state = 'character'
     bot.send_message(message.chat.id, 'Напишите персонажа, и главу, и ,возможно, название книги.')
 
 @bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
 def handle_text_doc(message):
-    if message1 == 'quotation':
+    if state == 'quotation':
         document_id = message.document.file_id
-        file_info = bot.get_file(document_id)
+        file_id_info = bot.get_file(document_id)
         downloaded_file = bot.download_file(file_id_info.file_path)
-        src = massage.document.file_name
-        with open('C:\Users\misha\PycharmProjects\QuotationChatBot' + "\" + src, 'wb') as new_file:
-            new_file.write(downloaded_file)    
-        #text = open('war-peace.txt', encoding="utf-8").read()
-    #elif message1 == 'character':
+        src = message.document.file_name
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        paragraphs = search_qoutation(qoutation, src)
     
     else:
         bot.send_message(message.chat.id, 'Извините, но Вы прислали это не вовремя.')
@@ -42,9 +50,14 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Привет! Как я могу Вам помочь?')
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Пока, хорошего дня!')
-    elif message1 == 'quotation':
+    elif state == 'quotation':
+        quotation = message
         text = open('war-peace.txt', encoding="utf-8").read()
         #print(text.lower().find(message.text.lower()))
-    elif message1 == 'character':
+    elif state == 'character':
+        character = message
         text = open('war-peace.txt', encoding="utf-8").read()
-bot.polling()
+if __name__ == '__main__':
+    quotation = 'до свидания'
+    print(search_qoutation(quotation, 'war-peace.txt'))
+#bot.polling()
